67b15c9355faf4d4c7e261166d1ee5453a27aee0
chowi <j.rojas.dtb@gmail.com>
2019-07-04 00:09:03 -0500

    initial commit

---
+++ app/src/main/java/com/samuelchowi/bellatech/main/custom/NavigationController.kt
@@ -0,0 +1,10 @@
+package com.samuelchowi.bellatech.main.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class NavigationController(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+
+
+}
